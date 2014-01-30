2b95cc65c80bb3291d90ca88d9be4fe28e2d0d35
Yaakov Gesher <yaakov@gesher.net>
2014-01-30 16:04:45 +0200

    2nd

---
+++ hebEngCal.php
@@ -0,0 +1,19 @@
+﻿
+<!DOCTYPE html>
+<html>
+	<head>
+	
+	</head>
+	<body>
+<?php
+class HeEnEventCal{
+	function __construct($eventsData,$dateFormat,$linkURL,$days=60, $hebTitles=TRUE){
+	
+	}
+	$hebMonths = array("January"=>"ינואר","February"=>"פברואר","March"=>"מרץ","April"=>"אפריל","May"=>"מאי","June"=>"יוני",
+	"July"=>"יולי","August"=>"אוגוסט","September"=>"ספטמבר","October"=>"אוקטובר","November"=>"נובמבר","December"=>"דצמבר" );
+}
+echo "שדגכעיחל";
+?>
+	</body>
+</html>
